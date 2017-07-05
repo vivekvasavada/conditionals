710c0c56c20538393216e257a164efbab1b0de7b
Vivek Vasavada <vivek.vasavada@gmail.com>
2017-07-05 11:34:41 -0400

    initial commit

---
+++ arrayspractice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayspractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //declaring without initializing
+           // int[] myFirstArray;
+            //declaring and initializing a second array
+          //  string[] myStringArray = { "first", "second", "third", "fourth" };
+            //declaring and initializing a char array with last name
+            //char[] lastName = { 'v', 'a', 's', 'a', 'v', 'a', 'd', 'a' };
+            //Console.WriteLine(lastName[0]);
+            //Console.WriteLine(lastName[1]);
+            //Console.WriteLine(lastName[2]);
+            //create following arrays and print using the correct index numbers
+            //Array of Top 10 Vacation spots
+            //print the ages of 3 children you know
+            //Array of gpa's on a 4.0 scale of 8 students
+            //Array of classmates' names who are sitting next to you
+            //print all four on the same line
+
+            //string[] vacationSpots = { "Cooperstown", "Bahamas", "Nyc", "Jamaica", "France", "North Carolina", "Austin", "Seattle", "Boston","Toronto" };
+            //Console.WriteLine(vacationSpots[0]);
+            //Console.WriteLine(vacationSpots[9]);
+            int[] childrenAges = {5,3,4};
+            Console.WriteLine(childrenAges[0]);
+            Console.WriteLine(childrenAges[1]);
+            Console.WriteLine(childrenAges[2]);
+        }
+    }
+}
--- arrayspractice/Properties/AssemblyInfo.cs
+++ arrayspractice/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Conditionals")]
+[assembly: AssemblyTitle("arrayspractice")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Conditionals")]
+[assembly: AssemblyProduct("arrayspractice")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c246c4e7-fb1e-488f-905b-9ae57ce4b59d")]
+[assembly: Guid("cd2e7cc2-aa48-4ac1-a4ce-f52e6de88957")]
 
 // Version information for an assembly consists of the following four values:
 //
